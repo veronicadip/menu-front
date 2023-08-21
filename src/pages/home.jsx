6f3e6018ed2fdd40a51945ffe841d5a67c59516c
tania <tania.vanina114@gmail.com>
2023-08-21 12:50:36 -0300

    modificaciones carrusel

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Carousel from 'react-bootstrap/Carousel';
 import { Carrusel } from '../components/carrusel';
 import NavbarComponent from '../components/navbar';
-import Menu from '../components/Menu';
+/*import Menu from '../components/Menu';*/
 import { Nosotras } from '../components/Nosotras';
 /*import ExampleCarouselImage from 'components/ExampleCarouselImage';*/
 
