a494e5d7fa7f004d5b4f7a6c8855500cbfd6287a
MacaCoronel93 <macarenajecoronel@gmail.com>
2023-08-22 23:20:36 -0300

    cambios en home

--- src/pages/homeScreem.jsx
+++ src/pages/homeScreem.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
+//import Carousel from 'react-bootstrap/Carousel';
 import { Carrusel } from '../components/carrusel';
 //import NavbarComponent from '../components/navbar';
 import Menu from '../components/Menu';
 /*import { Carrusel } from '../components/carrusel';*/
 /*import Menu from '../components/Menu';*/
 import { Nosotras } from '../components/Nosotras';
-import Menu from '../components/Menu';
-import Carrusel from '../components/carrusel';
+//import Menu from '../components/Menu';
+//import Carrusel from '../components/carrusel';
 /*import ExampleCarouselImage from 'components/ExampleCarouselImage';*/
 
 
