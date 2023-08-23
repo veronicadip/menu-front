d32deb6311e3ba8301ea2e08b85045953ca96e41
tania <tania.vanina114@gmail.com>
2023-08-22 21:05:22 -0300

    funcionamirnto

--- src/pages/homeScreem.jsx
+++ src/pages/homeScreem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Carousel from 'react-bootstrap/Carousel';
-import { Carrusel } from '../components/carrusel';
-import NavbarComponent from '../components/navbar';
+/*import { Carrusel } from '../components/carrusel';*/
 /*import Menu from '../components/Menu';*/
 import { Nosotras } from '../components/Nosotras';
+import Menu from '../components/menu';
+import Carrusel from '../components/carrusel';
 /*import ExampleCarouselImage from 'components/ExampleCarouselImage';*/
 
 
@@ -11,7 +11,6 @@ const  Home = () => {
   
   return (
     <>
-    <NavbarComponent/>
     <Carrusel/>
     <Menu/>
     <Nosotras/>
