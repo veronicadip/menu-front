a2325b505b5d008c54d915b68dcd90ec6fb7cb51
MacaCoronel93 <macarenajecoronel@gmail.com>
2023-08-24 19:17:13 -0300

    cambios

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Navbar,Nav,Container,Button } from 'react-bootstrap';
 //import { useState } from 'react';
-import  NavLink  from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Rutas from '../routes/rutas';
 import "../styles/navbar.css"
 
